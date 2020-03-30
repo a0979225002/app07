e47ba5ddaa4afb1cc9bca2a3178644ec1122238b
蕭立品 <a0979225002@gmail.com>
2020-03-30 13:46:09 +0800

    後臺寫出json格式,讓android能夠抓取sql資料

--- app/src/main/java/com/example/lipin/javaee_servlet/MainApp.java
+++ app/src/main/java/com/example/lipin/javaee_servlet/MainApp.java
@@ -6,7 +6,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 
 
-//讓全愈能共享Volley,目的不用在每個activity都實作出RequestQueue queue;
+//讓全域能共享Volley,目的不用在每個activity都實作出RequestQueue queue;
 public class MainApp extends Application {
     public static RequestQueue queue;
 
